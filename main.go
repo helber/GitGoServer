50e9c1909c2e2104500314dfb635496cde160125
helmutkemper <helmut.kemper@gmail.com>
2017-01-09 08:17:05 -0200

    trabalhando

--- main.go
+++ main.go
@@ -11,12 +11,10 @@ import (
   "github.com/gorilla/mux"
   "github.com/gorilla/sessions"
   "time"
-  "github.com/helmutkemper/GoSemanticUI/smt"
   "github.com/helmutkemper/gOsmServer/Install"
-  "github.com/helmutkemper/GoSemanticUI/smt/smtTemplate"
   "github.com/helmutkemper/gOsmServer/leaflet"
-  "gopkg.in/mgo.v2/bson"
   "github.com/helmutkemper/gOsm"
+  "runtime"
 )
 
 type RoutesStt []restFul.RouteStt
@@ -34,12 +32,12 @@ func init(){
       Pattern: "/parserratio",
       HandlerFunc: ParserRatio,
     },
-    restFul.RouteStt{
+    /*restFul.RouteStt{
       Name: "Index",
       Method: "GET",
       Pattern: "/",
       HandlerFunc: Index,
-    },
+    },*/
 
     // Download osm xml from geofabrik
     // json to send: { "continent": string, "name": string }
@@ -74,6 +72,14 @@ func init(){
       Method: "GET",
       Pattern: "/progressdownloadosm",
       HandlerFunc: install.ProgressDownloadOsmFile,
+    },
+
+
+    restFul.RouteStt{
+      Name: "ProgressDownloadOsm",
+      Method: "GET",
+      Pattern: "/memory",
+      HandlerFunc: MemoryAlloc,
     },
 
 
@@ -155,88 +161,15 @@ func ParserRatio(w http.ResponseWriter, r *http.Request) {
 
 
 
+func MemoryAlloc(w http.ResponseWriter, r *http.Request) {
+  var mem runtime.MemStats
+  runtime.ReadMemStats(&mem)
+
+  output := restFul.JSonOutStt{}
+  output.ToOutput( 1, nil, mem, w )
+}
+
 func Index(w http.ResponseWriter, r *http.Request) {
-  var smtHeader smt.Header
-
-  smtHeader = smt.Header{}
-
-  smtHeader.SetTitle( "Mapa" )
-  smtHeader.AddFw()
-  smtHeader.AddMap( "map", -27.427747,-48.45936, 17 )
-
-  session, err := store.Get(r, "demo")
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
-
-  if session.Values["uniqueId"] == nil {
-    session.Values["uniqueId"] = bson.NewObjectId().Hex()
-    err = session.Save(r, w)
-    if err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-      return
-    }
-  }
-
-  t := smtTemplate.Template{
-    Name: "index",
-    Out: w,
-  }
-
-  /*var icon smt.Icon = smt.Icon{
-    Name: smt.ICON_NAME_ADD_CIRCLE,
-    Variations: []smt.Variation{ smt.ICON_VARIATION_BORDERED },
-    Color: smt.COLOR_BLUE,
-    Size: smt.SIZE_BIG,
-  }
-
-  t.ParserString( icon.Get() )
-  t.ExecuteTemplate( "icon", icon )*/
-
-  var button smt.Button = smt.Button{
-    Label: "Ok",
-    Primary: true,
-    ContentHidden: smt.Content{
-      RightIcon: true,
-      Text: "Mundo",
-      Icon: smt.Icon{
-        Name: smt.ICON_NAME_ADD_CIRCLE,
-        Variations: []smt.Variation{ smt.ICON_VARIATION_BORDERED },
-        Color: smt.COLOR_BLUE,
-        Size: smt.SIZE_NORMAL,
-      },
-    },
-    ContentVisible: smt.Content{
-      RightIcon: false,
-      Text: "Olá",
-      Icon: smt.Icon{
-        Name: smt.ICON_NAME_ADD_CIRCLE,
-        Variations: []smt.Variation{smt.ICON_VARIATION_BORDERED },
-        Color: smt.COLOR_BLUE,
-        Size: smt.SIZE_NORMAL,
-      },
-    },
-  }
-
-  t.ParserString( button.String() )
-  t.ExecuteTemplate( "button", button )
-
-
-  //t.Execute( icon )
-  //t.ParserFiles(
-  //  "./templates/index/header.html",
-  //)
-  //t.ExecuteTemplate( "header", nil )
-
-
-}
-
-func TodoIndex(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func TodoCreate(w http.ResponseWriter, r *http.Request) {
 
 }
 
