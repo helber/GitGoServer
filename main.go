3c4ab5774dde2a95242bd6c657897a38d0e1bd6d
helmutkemper <helmut.kemper@gmail.com>
2017-07-12 19:01:54 -0300

    trabalhando

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ package main
  */
 
 //todo rever chaves do banco - urgente
+//todo ibge polygons deve ter a distância entre os pontos
 //todo geogson tem que ter box para banco com os 4 pontos, tem que ter o tipo original ( node way e relação ), centroid ou ponto na forma de loc - urgente
 //todo separar tag - urgente
 //todo box tem que ter os quatro pontos - urgente
